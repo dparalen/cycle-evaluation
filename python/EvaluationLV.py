122a64c31d20abc7f2631c85b304f72f4fea98f1
milan <vetrisko@gmail.com>
2011-10-17 23:48:37 +0200

    clean-up

--- python/EvaluationLV.py
+++ python/EvaluationLV.py
@@ -5,33 +5,8 @@ from Filter import ChFilter
 from numpy import *
 from multiprocessing import Pool
 from matplotlib import pyplot
+from Histogram import Histogram2D
 import matplotlib
-import copy_reg
-import types
-
-class Histogram2D:
-	def __init__(self, a, xlabel='length: l', ylabel='start: s',
-			title='s/l distribution'):
-		self.xmax, self.ymax = amax(a.T, axis=0)
-		self.histogram, self.xedges, self.yedges = histogram2d(a[0], a[1],
-				normed = True,
-			bins=[self.xmax, self.ymax])
-		self.extent = [self.yedges[0], self.yedges[-1], self.xedges[-1],
-				self.xedges[0]]
-		self.xlabel = xlabel
-		self.ylabel = ylabel
-		self.title = title
-	@property
-	def plot(self):
-		im = pyplot.imshow(self.histogram, extent=self.extent,
-				norm=matplotlib.colors.Normalize(vmin=0.,vmax=1.),
-				interpolation='bicubic'
-		)
-		im.axes.set_ylabel(self.ylabel)
-		im.axes.set_xlabel(self.xlabel)
-		im.axes.set_title(self.title)
-		im.colorbar = pyplot.colorbar()
-		return im
 
 def simMap(X):
 	return LotkaVolterra2D(X, timeSpace)
@@ -40,29 +15,46 @@ def fltMap(sim):
 	return ChFilter(sim.result.T, filter_)
 
 def cycMap(flt):
-	return CycleDetect(flt.result, epsilon=0.1)
+	return CycleDetect(flt.result, epsilon=0.3)
 
 if __name__ == "__main__":
 	# create a regular grid of init value combinations
-	iX, iY = meshgrid(range(1,11), range(1,11))
+	iX, iY = meshgrid(linspace(1,11,100), linspace(1,11,100))
 	Z = array([iX, iY])
 	# make it a "list of pairs"
-	Z.shape = 2, 100
+	Z.shape = 2, 10000
 	# create a time "axis"
 	timeSpace = linspace(0, 10, 2000)
 	# create a filter representation
-	filter_ = array([[5.0, 2.5, pi], [2.5, 5.0, pi]])
+	filter_ = array([linspace(1, 11, 10),[1., 2.]])
 	p = Pool(processes=16)
 	simData = p.map(simMap, Z.T, chunksize=8)
 	fltData = p.map(fltMap, simData, chunksize=8)
 	cycData = p.map(cycMap, fltData, chunksize=8)
 	result = array([array([x.start, x.length]) for x in cycData])
+	# some stats
+	starts = {}
+	lengths = {}
+	sl = {}
+	for x, y in result:
+		try:
+			starts[x] += 1
+		except KeyError:
+			starts[x] = 1
+		try:
+			lengths[y] += 1
+		except KeyError:
+			lengths[y] = 1
+		try:
+			sl["%s_%s" % (x,y)] += 1
+		except KeyError:
+			sl["%s_%s" % (x, y)] = 1
 	# create a histogram of the data
 	histogram = Histogram2D(result.T)
 	hplot = histogram.plot
 	#print histogram.histogram
+	print starts
+	print lengths
+	print sl
+	print len(sl)
 	pyplot.show()
-
-
-
-
--- python/EvaluationWH.py
+++ python/EvaluationWH.py
@@ -20,14 +20,14 @@ def cycMap(flt):
 if __name__ == "__main__":
 	logging.basicConfig(level=logging.DEBUG)
 	# create a regular grid of init value combinations
-	iX, iY = meshgrid(linspace(0., 6., 10), linspace(0., 6., 10))
+	iX, iY = meshgrid(linspace(0., 6., 3), linspace(0., 6., 3))
 	Z = array([iX, iY])
 	# make it a "list of pairs"
-	Z.shape = 2, 100
+	Z.shape = 2, 9
 	# create a time "axis"
-	timeSpace = linspace(0, 10, 2000)
+	timeSpace = linspace(0, 2, 2000)
 	# create a filter representation
-	filter_ = array([[3., 4., 5.],[3., 4., 5.]])
+	filter_ = array([[4., 4.5, 5., 5.5, 6., 6.5, 7, 7.5],[1]])
 	p = Pool(processes=16)
 	simData = p.map(simMap, Z.T, chunksize=8)
 	fltData = p.map(fltMap, simData, chunksize=8)
@@ -36,7 +36,7 @@ if __name__ == "__main__":
 	for x in simData:
 		pylab.plot(x.result.T[0], x.result.T[1], 'g.')
 	for x in fltData:
-		pylab.plot(x.result[0], x.result[0], 'r.')
+		pylab.plot(x.result[0], x.result[1], 'r.')
 	pyplot.show()
 	# create a histogram of the data
 	histogram = Histogram2D(result.T)
